8bf87cfac6ed6f623d100ab3aaff14fa10661c09
MUSALA\georgi.kotorov <georgi.kotorov@musala.com>
2017-03-31 18:07:03 +0300

    add custom drink

---
+++ src/prod/CustomDrink.java
@@ -0,0 +1,10 @@
+package prod;
+
+public class CustomDrink extends AbstractDrink {
+
+	public CustomDrink(String name, int water, int milk, int cofee, int cacao,
+			int shugar, int money) {
+		super(name, water, milk, cofee, cacao, shugar, money);
+	}
+
+}
